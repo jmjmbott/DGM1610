6f24bddd0ba0eed47476ea8af0f5bd53ed5e346e
Jordan <hunkyjerky@msn.com>
2021-06-03 10:05:13 -0600

    Test Variable and Debug
    Second test

---
+++ Assets/VariablesAndFunctions.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesAndFunctions : MonoBehaviour
+{
+    int myInt = 5;
+
+
+    void Start()
+    {
+        myInt = 55;
+        Debug.Log(myInt);
+    }
+}
