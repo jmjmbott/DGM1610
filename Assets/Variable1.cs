3159f72eb68c62fb687ec615d3e474565ae57698
Jordan <hunkyjerky@msn.com>
2021-06-11 22:00:56 -0600

    Long Variable
    Test

---
+++ Assets/Variable1.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variable1 : MonoBehaviour
+{
+    long howlong = 1500000000L;
+    void Start()
+    {
+        Debug.Log(howlong);
+    }
+}
