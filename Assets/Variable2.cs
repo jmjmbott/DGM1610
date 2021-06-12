b0c45a0bde15af0f865795a2dc6e657612b042e2
Jordan <hunkyjerky@msn.com>
2021-06-11 22:02:59 -0600

    String demo

---
+++ Assets/Variable2.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variable2 : MonoBehaviour
+{
+    string blubblernuff = "Wow did I get this right?";
+
+    void Start()
+    {
+        Debug.Log (blubblernuff);
+        
+    }
+}
