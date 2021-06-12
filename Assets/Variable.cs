62f3334a89b584f7d667e9cfcbf09c2a284aa047
Jordan <hunkyjerky@msn.com>
2021-06-11 21:22:47 -0600

    Variable test
    Created new variable

---
+++ Assets/Variable.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variable : MonoBehaviour
+{
+    int x = 50;
+        
+    void Start()
+    { int sum1 = x + 25;
+        Debug.Log(sum1);
+        
+    }
+}
