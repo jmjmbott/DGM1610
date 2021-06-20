3e04edb622a79dbe6cf6973a5b7fb94e206e02bc
Jordan <hunkyjerky@msn.com>
2021-06-19 22:04:47 -0600

    Conditional
    Posted result of equation

---
+++ Assets/ConditionalTST.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConditionalTST : MonoBehaviour
+{   int x = 20;
+    int y = 55;
+    void Start()
+    {
+        Debug.Log("well howdy!");
+        if (x > y) ;
+        
+    }
+}
