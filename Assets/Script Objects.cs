3b0f7f289f8d75e5606b38b16313db1cf3775d51
Jordan <hunkyjerky@msn.com>
2021-08-05 22:54:28 -0600

    ScriptableObjects
    Tested scriptable objects

---
+++ Assets/Script Objects.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Thing", menuName = "Things")]
+public class ScriptObjects : ScriptableObject
+{ 
+    public new string name;
+    public string description;
+
+    public int number;
+    public int otherThing;
+    
+
+
+        
+    
+}
